c6e8f1091a37210d3dc19ebdfb573c75822376f9
sotero1 <sotero1@hotmail.com>
2014-10-10 15:26:51 -0700

    "plot 1 commit"

--- EDAProject1.R
+++ EDAProject1.R
@@ -10,7 +10,27 @@ econsumption <- read.table("household_power_consumption.txt",sep = ";", header =
 # change as.date format to day month year before converting below
 #as.Date(dates, "%m/%d/%y")
 
-econ<- data.frame(apply(econsumption[1],2, as.Date, "%d/%m/%y"),econsumption[2:9])
-ds
-f'sa
-d'
+
+
+econ<- data.frame(apply(econsumption[1],2, as.Date, format="%d/%m/%y"),econsumption[2:9])
+
+
+#18293=feb 1 2006
+#18294=feb 2 2006
+
+
+as.Date("01/02/2006","%d/%m/%y")
+
+econs<-econ[econ$Date==18293,  ]
+
+econs2<-econ[econ$Date==18294,  ]
+
+econs3<-rbind(econs,econs2)
+
+econs3$Global_active_power<-as.numeric(econs3$Global_active_power)
+
+econs3$Global_active_power<-econs3$Global_active_power/1000
+
+hist(econs3$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power" )
+
+
